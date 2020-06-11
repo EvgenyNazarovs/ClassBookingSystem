995503b3231cc9d971f936e3e5491599e7dcc471
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-06-11 12:13:00 +0100

    adds booking and course models

---
+++ src/main/java/com/github/EvgenyNazarovs/CourseBookingSystem/models/Booking.java
@@ -0,0 +1,50 @@
+package com.github.EvgenyNazarovs.CourseBookingSystem.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String date;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    public Booking(String date, Course course) {
+        this.date = date;
+        this.course = course;
+    }
+
+    public Booking() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
