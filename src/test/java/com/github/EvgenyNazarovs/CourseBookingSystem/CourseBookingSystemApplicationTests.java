15e89f2ff5fc0d970b02c2c5b94896dc96452426
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-06-11 13:08:18 +0100

    can get bookings by date and get courses by rating

--- src/test/java/com/github/EvgenyNazarovs/CourseBookingSystem/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/github/EvgenyNazarovs/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -1,5 +1,7 @@
 package com.github.EvgenyNazarovs.CourseBookingSystem;
 
+import com.github.EvgenyNazarovs.CourseBookingSystem.models.Booking;
+import com.github.EvgenyNazarovs.CourseBookingSystem.models.Course;
 import com.github.EvgenyNazarovs.CourseBookingSystem.repositories.BookingRepository;
 import com.github.EvgenyNazarovs.CourseBookingSystem.repositories.CourseRepository;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,10 +29,17 @@ class CourseBookingSystemApplicationTests {
 	void contextLoads() {
 	}
 
-//	@Test
-//	public void canGetCoursesByRating() {
-//
-//	}
+	@Test
+	public void canGetCoursesByRating() {
+		List<Course> found = courseRepository.findByStarRating(4.5);
+		assertEquals(1, found.size());
+	}
+
+	@Test
+	public void canGetBookingsByDate() {
+		List<Booking> found = bookingRepository.findByDate("20-06-2020");
+		assertEquals(1, found.size());
+	}
 
 
 
